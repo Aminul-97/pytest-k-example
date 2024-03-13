d39fbe5186c7842f2e8b4b2b38d830caf04eb510
Aminul-97 <amin147570bd@gmail.com>
2024-03-13 20:06:53 +0600

    New example code

---
+++ tests/test_system_network_module.py
@@ -0,0 +1,15 @@
+# Testing system core networking module
+def test_system_core_network():
+    assert True
+
+# Testing system http
+def test_system_network_http():
+    assert True
+
+# Testing system network status 
+def test_system_network_status():
+    assert True
+
+# Testing system network sync
+def test_system_network_sync():
+    assert True
